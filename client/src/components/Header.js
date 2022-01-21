20e1b3b4f58454310e7c5821958aaea18fe7cf09
Abby Anderson <abby.e.anderson@gmail.com>
2022-01-20 17:06:49 -0700

    updated styling on app and image cards, added animation to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header () {
     return (
-        <div className="header"> 
-            <p>Spacestagram</p>
-            <p>Brought to you by NASA's Astronomy Picture of the Day</p>
+        <div className="header focus-in-contract"> 
+            <p id="title">Spacestagram</p>
+            <p id="subtitle">Brought to you by NASA's Astronomy Picture of the Day</p>
         </div>
 
     );
